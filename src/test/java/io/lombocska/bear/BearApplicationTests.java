e084ec37b9f734a9f6181bb835314a25b9606045
lombocska <lombos.monika@gmail.com>
2019-06-11 08:52:03 +0200

    feat: added docker images for integration testing

--- src/test/java/io/lombocska/bear/BearApplicationTests.java
+++ src/test/java/io/lombocska/bear/BearApplicationTests.java
@@ -2,11 +2,14 @@ package io.lombocska.bear;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("it")
 public class BearApplicationTests {
 
     @Test
